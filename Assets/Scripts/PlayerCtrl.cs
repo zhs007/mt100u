8276fd007e30f18a8e1f83317e9c87b4417fc27f
ZerroZhao <zerrozhao@gmail.com>
2022-06-16 18:51:33 +0800

    upd

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    public Transform m_transform;
-    public float m_zAngle;
+    // public Transform m_transform;
+    public float zAngle;
     public float lastOffAngle;
-    public DynamicJoystick m_variableJoystick;
+    public DynamicJoystick joystick;
     public GameObject mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = GameObject.Find("Main Camera");
-        m_variableJoystick = GameObject.Find("Dynamic Joystick").GetComponent<DynamicJoystick>();
-        m_transform = this.transform;
-        m_zAngle = 0.0f;
+        joystick = GameObject.Find("Dynamic Joystick").GetComponent<DynamicJoystick>();
+        // m_transform = this.transform;
+        zAngle = 0.0f;
 
         mainCamera.transform.position.Set(transform.position.x, transform.position.y, mainCamera.transform.position.z);
     }
@@ -63,7 +63,7 @@ public class PlayerCtrl : MonoBehaviour
             }
             else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
             {
-                if (m_zAngle < 0)
+                if (zAngle < 0)
                 {
                     dstAngle = -180.0f;
                 }
@@ -74,79 +74,79 @@ public class PlayerCtrl : MonoBehaviour
             }
             else
             {
-                dstAngle = m_zAngle;
+                dstAngle = zAngle;
             }
         }
 
-        if (Mathf.Abs(dstAngle - m_zAngle) > 180)
+        if (Mathf.Abs(dstAngle - zAngle) > 180)
         {
-            if (m_zAngle < -180)
+            if (zAngle < -180)
             {
-                m_zAngle += 360.0f;
+                zAngle += 360.0f;
             }
-            else if (m_zAngle > 180)
+            else if (zAngle > 180)
             {
-                m_zAngle -= 360;
+                zAngle -= 360;
             }
             else if (dstAngle < 0)
             {
-                m_zAngle -= 360;
+                zAngle -= 360;
             }
             else if (dstAngle > 0)
             {
-                m_zAngle += 360;
+                zAngle += 360;
             }
         }
 
 
         float offAngle = 0.0f;
-        if (m_zAngle > dstAngle)
+        if (zAngle > dstAngle)
         {
             offAngle = -90.0f * Time.deltaTime;
-            if (m_zAngle + offAngle <= dstAngle)
+            if (zAngle + offAngle <= dstAngle)
             {
-                offAngle = dstAngle - m_zAngle;
-                m_zAngle = dstAngle;
+                offAngle = dstAngle - zAngle;
+                zAngle = dstAngle;
             }
             else
             {
-                m_zAngle += offAngle;
+                zAngle += offAngle;
             }
         }
-        else if (m_zAngle < dstAngle)
+        else if (zAngle < dstAngle)
         {
             offAngle = 90.0f * Time.deltaTime;
-            if (m_zAngle + offAngle >= dstAngle)
+            if (zAngle + offAngle >= dstAngle)
             {
-                offAngle = dstAngle - m_zAngle;
-                m_zAngle = dstAngle;
+                offAngle = dstAngle - zAngle;
+                zAngle = dstAngle;
             }
             else
             {
-                m_zAngle += offAngle;
+                zAngle += offAngle;
             }
         }
 
-        m_transform.Rotate(new Vector3(0, 0, offAngle), Space.Self);
+        transform.Rotate(new Vector3(0, 0, offAngle), Space.Self);
 
         if (Input.GetKey(KeyCode.A))
         {
-            m_transform.position += new Vector3(-1f * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(-1f * Time.deltaTime, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            m_transform.position += new Vector3(1f * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(1f * Time.deltaTime, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            m_transform.position += new Vector3(0, 1f * Time.deltaTime, 0);
+            transform.position += new Vector3(0, 1f * Time.deltaTime, 0);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            m_transform.position += new Vector3(0, -1f * Time.deltaTime, 0);
+            transform.position += new Vector3(0, -1f * Time.deltaTime, 0);
         }
     }
 
@@ -155,23 +155,23 @@ public class PlayerCtrl : MonoBehaviour
     {
         ProcKeyboard();
 
-        if (m_variableJoystick.Horizontal == 0 && m_variableJoystick.Vertical == 0)
+        if (joystick.Horizontal == 0 && joystick.Vertical == 0)
         {
             return;
         }
 
         // float offAngle = Vector3.Angle(Quaternion.AngleAxis(m_zAngle, Vector3.up) * Vector3.up, new Vector3(m_variableJoystick.Horizontal, m_variableJoystick.Vertical, 0));
         // float offAngle = Vector3.Angle(new Vector3(Mathf.Sin(m_zAngle * Mathf.Deg2Rad), Mathf.Cos(m_zAngle * Mathf.Deg2Rad), 0), new Vector3(m_variableJoystick.Horizontal, m_variableJoystick.Vertical, 0));
-        float offAngle = Vector3.SignedAngle(new Vector3(Mathf.Sin(m_zAngle * Mathf.Deg2Rad), Mathf.Cos(m_zAngle * Mathf.Deg2Rad), 0), new Vector3(m_variableJoystick.Horizontal, -m_variableJoystick.Vertical, 0), Vector3.forward);
+        float offAngle = Vector3.SignedAngle(new Vector3(Mathf.Sin(zAngle * Mathf.Deg2Rad), Mathf.Cos(zAngle * Mathf.Deg2Rad), 0), new Vector3(joystick.Horizontal, -joystick.Vertical, 0), Vector3.forward);
         // if ((offAngle < 0 && lastOffAngle < 0) || (offAngle > 0 && lastOffAngle > 0))
         {
             offAngle *= Time.deltaTime;
             // if (Mathf.Abs(offAngle) > 1)
             {
-                m_transform.Rotate(new Vector3(0, 0, offAngle), Space.Self);
-                m_zAngle += offAngle;
+                transform.Rotate(new Vector3(0, 0, offAngle), Space.Self);
+                zAngle += offAngle;
 
-                m_zAngle %= 360;
+                zAngle %= 360;
             }
         }
 
@@ -183,11 +183,11 @@ public class PlayerCtrl : MonoBehaviour
         // m_transform.position += new Vector3(0, m_variableJoystick.Vertical * Time.deltaTime, 0);
         // m_transform.position += new Vector3(m_variableJoystick.Horizontal * Time.deltaTime, 0, 0);
 
-        m_transform.position += new Vector3(-Mathf.Sin(m_zAngle * Mathf.Deg2Rad), Mathf.Cos(m_zAngle * Mathf.Deg2Rad), 0) * Time.deltaTime;
+        transform.position += new Vector3(-Mathf.Sin(zAngle * Mathf.Deg2Rad), Mathf.Cos(zAngle * Mathf.Deg2Rad), 0) * Time.deltaTime;
 
-        mainCamera.transform.position += new Vector3(-Mathf.Sin(m_zAngle * Mathf.Deg2Rad), Mathf.Cos(m_zAngle * Mathf.Deg2Rad), 0) * Time.deltaTime;
+        mainCamera.transform.position += new Vector3(-Mathf.Sin(zAngle * Mathf.Deg2Rad), Mathf.Cos(zAngle * Mathf.Deg2Rad), 0) * Time.deltaTime;
 
-        Debug.Log("player - " + transform.position);
-        Debug.Log("camera - " + mainCamera.transform.position);
+        // Debug.Log("player - " + transform.position);
+        // Debug.Log("camera - " + mainCamera.transform.position);
     }
 }
