0fc9dd439adc4e809aaa8a0762ccca5c45692340
zhs007 <sssxueren@gmail.com>
2022-06-17 16:13:33 +0800

    upd

---
+++ Assets/Scripts/MapObjs.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObjs : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    public int
+
+            minX,
+            minY,
+            maxX,
+            maxY;
+
+    public int nums;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<Vector2> lst = new List<Vector2>();
+
+        for (int i = 0; i < nums; ++i)
+        {
+            int tx = Random.Range(minX, maxX);
+            int ty = Random.Range(minY, maxY);
+
+            while (true)
+            {
+                if (lst.IndexOf(new Vector2(tx, ty)) < 0)
+                {
+                    break;
+                }
+
+                tx = Random.Range(minX, maxX);
+                ty = Random.Range(minY, maxY);
+            }
+
+            int r = Random.Range(0, prefabs.Length);
+            lst.Add(new Vector2(tx, ty));
+
+            Instantiate(prefabs[r],
+            new Vector3(tx, ty, 0),
+            Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
